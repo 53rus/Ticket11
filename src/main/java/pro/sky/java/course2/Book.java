5538120d112faf935045b1f41c8b46829256ed14
53rus <53rus@mail.ru>
2024-07-27 17:49:33 +0300

    Билет №11 Задача:Дан массив пар названий книг и авторов, необходимо привести каждую пару в строку вида: «Название книги» Имя Отчество Фамилия автора.

---
+++ src/main/java/pro/sky/java/course2/Book.java
@@ -0,0 +1,31 @@
+package pro.sky.java.course2;
+
+public class Book {
+    String bookName;
+    String author;
+
+    public Book(String bookName, String author) {
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String toString() {
+        return "Название книги: " + getBookName() + ", Имя Отчество автора: " + getAuthor();
+    }
+}
---
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -0,0 +1,12 @@
+package pro.sky.java.course2;
+
+public class Main {
+    public static void main(String[] args) {
+        Book[] books = new Book[]{
+                new Book("Книга 1", "Автор 1"),
+                new Book("Book 2", "Author 2")
+        };
+        System.out.println(books[0]);
+        System.out.println(books[1]);
+    }
+}
