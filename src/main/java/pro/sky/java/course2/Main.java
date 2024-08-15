d1b2779f764e95b1d44eff05f6fe9857de898094
53rus <53rus@mail.ru>
2024-08-15 12:20:59 +0300

    demo-feature

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Book[] books = new Book[]{
                 new Book("Книга 1", "Автор 1"),
-                new Book("Book 2", "Author 2")
+                new Book("Book 2", "Author 2"),
+                new Book("Book 3", "Author 3")
         };
         System.out.println(books[1]);
     }
