d8a1b62757f6b2b846f00e42d326701a1d8d3e74
53rus <53rus@mail.ru>
2024-08-16 21:46:47 +0300

    add new Book

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -6,7 +6,9 @@ public class Main {
                 new Book("Книга 1", "Автор 1"),
                 new Book("Book 2", "Author 2"),
                 new Book("Book 3", "Author 3"),
-                new Book("Book 4", "Author 4")
+                new Book("Book 4", "Author 4"),
+                new Book("Book 11", "Author 11"),
+                new Book("Book 121", "Author 121")
         };
         System.out.println(books[1]);
     }
