372e67674af03594d45c78e0d86b46c319b45281
53rus <151921769+53rus@users.noreply.github.com>
2024-08-16 21:30:37 +0300

    Update Main.java

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -6,7 +6,8 @@ public class Main {
                 new Book("Книга 1", "Автор 1"),
                 new Book("Book 2", "Author 2"),
                 new Book("Book 3", "Author 3"),
-                new Book("Book 4", "Author 4")
+                new Book("Book 4", "Author 4"),
+                new Book("Book 5", "Author 5")
         };
         System.out.println(books[1]);
     }
